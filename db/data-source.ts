03d5ad7b273cc4c6006117b0e553d92e343394e1
LevyNatenadze <levani.natenadze1@gmail.com>
2024-10-28 20:00:37 +0400

    feat: -users module. -create migration script

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,11 +10,11 @@ export const dataSourceOptions: DataSourceOptions = {
     username: process.env.DB_USERNAME,
     password: String(process.env.DB_PASSWORD),
     database: process.env.DB_DATABASE,
-    entities: [],
-    migrations: [],
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/db/migrations/*{.ts,.js}'],
     logging: false,
     synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
-export default dataSource;
+export default dataSource; 
